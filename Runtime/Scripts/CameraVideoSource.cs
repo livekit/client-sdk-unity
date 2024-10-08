0d513a89110cb15773d207635b6170f8a5d608dc
cloudwebrtc <duanweiwei1982@gmail.com>
2024-10-08 13:54:52 +0800

    fix: Optimize VideoSource capture and add WebCameraSource.

--- Runtime/Scripts/CameraVideoSource.cs
+++ Runtime/Scripts/CameraVideoSource.cs
@@ -8,9 +8,13 @@ using Unity.Collections;
 
 namespace LiveKit
 {
+
+    // VideoSource for Unity Camera
     public class CameraVideoSource : RtcVideoSource
     {
         private TextureFormat _textureFormat;
+
+        private RenderTexture _renderTexture;
 
         public Camera Camera { get; }
 
@@ -22,6 +26,11 @@ namespace LiveKit
         public override int GetHeight()
         {
             return Camera.pixelHeight;
+        }
+
+        protected override VideoRotation GetVideoRotation()
+        {
+            return VideoRotation._0;
         }
 
         public CameraVideoSource(Camera camera,   VideoBufferType bufferType = VideoBufferType.Rgba) : base(VideoStreamSource.Screen, bufferType)
@@ -44,9 +53,9 @@ namespace LiveKit
 
         private void ClearRenderTexture()
         {
-            if (_dest)
+            if (_renderTexture)
             {
-                var renderText = _dest as RenderTexture;
+                var renderText = _renderTexture as RenderTexture;
                 renderText.Release(); // can only be done on main thread
             }
         }
@@ -60,19 +69,35 @@ namespace LiveKit
             var textureChanged = false;
             try
             {
-                if (_dest == null || _dest.width != GetWidth() || _dest.height != GetHeight())
+                if (_renderTexture == null || _renderTexture.width != GetWidth() || _renderTexture.height != GetHeight())
                 {
                     var targetFormat = Utils.GetSupportedGraphicsFormat(SystemInfo.graphicsDeviceType);
                     var compatibleFormat = SystemInfo.GetCompatibleFormat(targetFormat, FormatUsage.ReadPixels);
                     _textureFormat = GraphicsFormatUtility.GetTextureFormat(compatibleFormat);
                     _bufferType = GetVideoBufferType(_textureFormat);
-                    _dest = new RenderTexture(GetWidth(), GetHeight(), 0, compatibleFormat);
-                    Camera.targetTexture = _dest as RenderTexture;
+                    _renderTexture = new RenderTexture(GetWidth(), GetHeight(), 0, compatibleFormat);
+                    Camera.targetTexture = _renderTexture as RenderTexture;
                     _data = new NativeArray<byte>(GetWidth() * GetHeight() * GetStrideForBuffer(_bufferType), Allocator.Persistent);
+                    _dest = new Texture2D(GetWidth(), GetHeight(), _textureFormat, false);
                     textureChanged = true;
                 }
-                ScreenCapture.CaptureScreenshotIntoRenderTexture(_dest as RenderTexture);
-                AsyncGPUReadback.RequestIntoNativeArray(ref _data, _dest, 0, _textureFormat, OnReadback);
+                ScreenCapture.CaptureScreenshotIntoRenderTexture(_renderTexture);
+
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+                // Flip the texture for OSX
+                Graphics.CopyTexture(_renderTexture, _dest);
+                var pixels = _dest.GetPixels();
+                var flippedPixels = new Color[pixels.Length];
+                for (int i = 0; i < _dest.height; i++)
+                {
+                    Array.Copy(pixels, i * _dest.width, flippedPixels, (_dest.height - i - 1) * _dest.width, _dest.width);
+                }
+                _dest.SetPixels(flippedPixels);
+#else
+                Graphics.CopyTexture(_renderTexture, _dest);
+#endif
+
+                AsyncGPUReadback.RequestIntoNativeArray(ref _data, _renderTexture, 0, _textureFormat, OnReadback);
             }
             catch (Exception e)
             {
