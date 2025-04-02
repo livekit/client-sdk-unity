6cd87d0301c0489413a375981234137be83546de
holofermes <holofermes@gmail.com>
2025-04-02 10:38:57 -0400

    add disconnect delegate, and fire it across all paths

--- Runtime/Scripts/Room.cs
+++ Runtime/Scripts/Room.cs
@@ -125,6 +125,7 @@ namespace LiveKit
         public delegate void SipDtmfDelegate(Participant participant, UInt32 code, string digit);
         public delegate void ConnectionStateChangeDelegate(ConnectionState connectionState);
         public delegate void ConnectionDelegate(Room room);
+        public delegate void DisconnectDelegate(DisconnectReason reason);
         public delegate void E2EeStateChangedDelegate(Participant participant, EncryptionState state);
 
         public string Sid { private set; get; }
@@ -152,7 +153,7 @@ namespace LiveKit
         public event SipDtmfDelegate SipDtmfReceived;
         public event ConnectionStateChangeDelegate ConnectionStateChanged;
         public event ConnectionDelegate Connected;
-        public event ConnectionDelegate Disconnected;
+        public event DisconnectDelegate Disconnected;
         public event ConnectionDelegate Reconnecting;
         public event ConnectionDelegate Reconnected;
         public event E2EeStateChangedDelegate E2EeStateChanged;
@@ -455,7 +456,7 @@ namespace LiveKit
                     ConnectionStateChanged?.Invoke(e.ConnectionStateChanged.State);
                     break;
                 case RoomEvent.MessageOneofCase.Disconnected:
-                    Disconnected?.Invoke(this);
+                    Disconnected?.Invoke(e.Disconnected.Reason);
                     OnDisconnect();
                     break;
                 case RoomEvent.MessageOneofCase.Reconnecting:
@@ -493,6 +494,8 @@ namespace LiveKit
 
         private void OnDisconnectReceived(DisconnectCallback e)
         {
+            OnDisconnect();
+            Disconnected?.Invoke(DisconnectReason.ClientInitiated);
             FfiClient.Instance.DisconnectReceived -= OnDisconnectReceived;
             Utils.Debug($"OnDisconnect.... {e}");
         }
