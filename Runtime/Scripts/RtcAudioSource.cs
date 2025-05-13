7e9decc5f99788f112095b5dad8d606b93c63759
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2025-05-13 22:17:41 +1000

    Add doc comment

--- Runtime/Scripts/RtcAudioSource.cs
+++ Runtime/Scripts/RtcAudioSource.cs
@@ -17,6 +17,9 @@ namespace LiveKit
         AudioSourceMicrophone = 1
     }
 
+    /// <summary>
+    /// Capture source for a local audio track.
+    /// </summary>
     public abstract class RtcAudioSource : IRtcSource, IDisposable
     {
         /// <summary>
